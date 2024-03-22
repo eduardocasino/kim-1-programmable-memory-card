4a948ab7fe298ac5bceda7dc65161fc2d5d870e5
Eduardo Casino <mail@eduardocasino.es>
2024-03-22 10:48:20 +0100

    New PIO code, now using RAMRW instead of RW

--- firmware/piocfg.c
+++ firmware/piocfg.c
@@ -25,6 +25,7 @@ static void piocfg_gpio_pins( PIO pio )
     for ( int pin = PIN_BASE_ADDR; pin < PIN_BASE_ADDR+16; ++pin ) {
         pio_gpio_init( pio, pin );
         gpio_set_dir( pin, GPIO_IN );
+        gpio_pull_down( pin );
     }    
 
     // Configure data bus pins initially as out pins
@@ -35,7 +36,7 @@ static void piocfg_gpio_pins( PIO pio )
             pio_gpio_init( pio, pin );
             gpio_set_dir( pin, GPIO_OUT );
         }
-    }   
+    }
 }
 
 static int piocfg_create_memread_sm( PIO pio )
@@ -69,18 +70,14 @@ static int piocfg_create_memwrite_sm( PIO pio )
     
     pio_sm_config memwrite_config = memwrite_program_get_default_config( memwrite_offset ); // Get default config for the memory write SM
 
-    // Configure: IN  pins: DATA    (PINCTRL_IN_BASE, PINCTRL_IN_COUNT)
-    //            JMP pin:  WR      (EXECCTRL_JMP_PIN)
-    //
-    sm_config_set_in_pins( &memwrite_config, CE );                                          // Pin set for IN and GET instructions. See FIXME note below
+    sm_config_set_in_pins( &memwrite_config, PIN_BASE_DATA );                               // Pin set for IN and GET instructions.
     sm_config_set_jmp_pin( &memwrite_config, RW );                                          // Pin for conditional JMP instructions
 
-    // FIXME: I'm assuming that reading CE, which is an putput pin, will get the correct value (which is 0)
-    //        CHECK THAT IT WORKS AND, IF NOT, RECONFIGURE in pins to PIN_BASE_DATA and shift a 0 before data in the pio code
-    //
-    sm_config_set_in_shift( &memwrite_config, false, false, 13 );                           // Shift left CE, data (8bit data + 3 unused) and RW into ISR, no autopush
+    sm_config_set_in_shift( &memwrite_config, false, true, 13 );                            // Shift left RW, data (8bit data + 3 unused) and CE into ISR, autopush
 
     pio_sm_set_pindirs_with_mask( pio, memwrite_sm, (1 << CE), (1 << CE)|(1 << RW) );       // Set CE as output, RW as input
+
+    pio_interrupt_clear ( pio, 7);
 
     pio_sm_init( pio, memwrite_sm, memwrite_offset, &memwrite_config );
 
@@ -105,45 +102,15 @@ void piocfg_setup( uint16_t *mem_map )
 
     // Configure the DMA channels
     //
-    // * Channel read_addr_dma:  Moves address from memread_sm RX FiFo ( addr bus combined with the mem_map base address) to the read_data_dma channel config read address trigger. Chains to write_addr_dma
-    // * Channel write_addr_dma: Moves address from read_data_dma channel config read address to the write_data_dma channel config write trigger address. Chains to read_addr_dma.
-    // * Channel read_data_dma:  Moves data from mem_map (as previosly set by read_addr_dma) to memread_sm TX FiFo. Does not chain.
-    // * Channel write_data_dma: Moves data from memwrite_sm RX FiFo to the mem_map addr configured by write_addr_dma.Does not chain.
+    // * Channel read_addr_dma:  Moves address from memread_sm RX FiFo ( addr bus combined with the mem_map base address) to the read_data_dma channel config read address. Chains to write_addr_dma
+    // * Channel write_addr_dma: Moves address from read_data_dma channel config read address to the write_data_dma channel config write trigger address. Chains to read_data_dma.
+    // * Channel read_data_dma:  Moves data from mem_map (as previosly set by read_addr_dma) to memread_sm TX FiFo. Chains to read_addr_dma
+    // * Channel write_data_dma: Moves data from memwrite_sm RX FiFo to the mem_map addr configured by write_addr_dma. Does not chain.
     //
     int read_addr_dma   = dma_claim_unused_channel( true );
     int read_data_dma   = dma_claim_unused_channel( true );
     int write_addr_dma  = dma_claim_unused_channel( true );
     int write_data_dma  = dma_claim_unused_channel( true );
-
-    dma_channel_config read_addr_dma_config = dmacfg_config_channel(
-                read_addr_dma,
-                pio_get_dreq( pio, memread_sm, false ),                     // Signals data transfer from PIO, receive
-                DMA_SIZE_32,
-                write_addr_dma,                                             // Chains to write_addr_dma when finished
-                &dma_channel_hw_addr(read_data_dma)->al3_read_addr_trig,    // Writes to read address trigger of read_data_dma
-                &pio->rxf[memread_sm],                                      // Reads from memread_sm RX FiFo
-                true                                                        // Starts immediately
-                );
-
-    dma_channel_config write_addr_dma_config = dmacfg_config_channel(
-                write_addr_dma,
-                DREQ_FORCE,                                                 // Permanent request transfer
-                DMA_SIZE_32,
-                read_addr_dma,                                              // Chains to read_addr_dma when finished
-                &dma_channel_hw_addr(write_data_dma)->al2_write_addr_trig,  // Writes to write address trigger of write_data_dma
-                &dma_channel_hw_addr(read_data_dma)->al3_read_addr_trig,    // Reads from read address trigger of read_data_dma
-                false                                                       // Does not start
-                );
-
-    dma_channel_config read_data_dma_config = dmacfg_config_channel(
-                read_data_dma,
-                pio_get_dreq( pio, memread_sm, true ),                      // Signals data transfer from PIO, transmit
-                DMA_SIZE_16,
-                read_data_dma,                                              // Does not chain (chain to itself means no chain)
-                &pio->txf[memread_sm],                                      // Writes memread_sm TX FiFo
-                mem_map,                                                    // Reads from mem_map (efective address configured by read_addr_dma)
-                false                                                       // Does not start
-                );
 
     dma_channel_config write_data_dma_config = dmacfg_config_channel(
                 write_data_dma,
@@ -155,15 +122,43 @@ void piocfg_setup( uint16_t *mem_map )
                 false                                                       // Does not start
                 );
 
-    // Enable State Machines
-    //
+    dma_channel_config read_data_dma_config = dmacfg_config_channel(
+                read_data_dma,
+                pio_get_dreq( pio, memread_sm, true ),                      // Signals data transfer from PIO, transmit
+                DMA_SIZE_16,
+                read_addr_dma,                                              // Chains to read_addr_dma
+                &pio->txf[memread_sm],                                      // Writes memread_sm TX FiFo
+                mem_map,                                                    // Reads from mem_map (efective address configured by read_addr_dma)
+                false                                                       // Does not start
+                );
+
+    dma_channel_config write_addr_dma_config = dmacfg_config_channel(
+                write_addr_dma,
+                DREQ_FORCE,                                                 // Permanent request transfer
+                DMA_SIZE_32,
+                read_data_dma,                                              // Chains to read_data_dma when finished
+                &dma_channel_hw_addr(write_data_dma)->al2_write_addr_trig,  // Writes to write address trigger of write_data_dma
+                &dma_channel_hw_addr(read_data_dma)->read_addr,             // Reads from read address trigger of read_data_dma
+                false                                                       // Does not start
+                );
+
+    dma_channel_config read_addr_dma_config = dmacfg_config_channel(
+                read_addr_dma,
+                pio_get_dreq( pio, memread_sm, false ),                     // Signals data transfer from PIO, receive
+                DMA_SIZE_32,
+                write_addr_dma,                                             // Chains to write_addr_dma when finished
+                &dma_channel_hw_addr(read_data_dma)->read_addr,             // Writes to read address of read_data_dma
+                &pio->rxf[memread_sm],                                      // Reads from memread_sm RX FiFo
+                true                                                        // Starts immediately
+                );
 
     // Put mem_map base address shifted 17 bits, so the 16 gpios + a '0' (multiplied by 2) gives the correct addr
     //
     pio_sm_put( pio, memread_sm, dma_channel_hw_addr( read_data_dma )->read_addr >> 17 );
     pio_sm_set_enabled( pio, memread_sm, true );
 
+    // Enable State Machines
+    //
     pio_sm_set_enabled( pio, memwrite_sm, true );
-
 }
 
